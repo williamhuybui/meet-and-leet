9ed6f05b5848289a6fd22ef7787ddd1ae1ec9fcd
williamhuybui <williamhuybui@gmail.com>
2025-09-27 22:23:14 -0500

    Add new algorithm

--- memorize_list.py
+++ memorize_list.py
@@ -4,4 +4,12 @@ def find_gcd_euclidean(a,b):
         a, b = b, a % b
     return a
 
-print(find_gcd_euclidean(1,30))
+def count_frequency(lst):
+    """Count frequency of elements in a list and return as a dictionary"""
+    freq = {}
+    for item in lst:
+        if item in freq:
+            freq[item] += 1
+        else:
+            freq[item] = 1
+    return freq
