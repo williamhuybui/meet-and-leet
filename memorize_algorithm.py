9de25fc874c9dd0be263aa7f918b509575f52f72
williamhuybui <williamhuybui@gmail.com>
2025-09-30 12:35:35 -0500

    Note

--- memorize_algorithm.py
+++ memorize_algorithm.py
@@ -1,10 +1,12 @@
 def find_gcd_euclidean(a,b):
+    """Topic: Math"""
     """Find GCD using Euclidean algorithm, a,b > 0"""
     while b:
         a, b = b, a % b
     return a
 
 def count_frequency(lst):
+    """Topic: Hash map"""
     """Count frequency of elements in a list and return as a dictionary"""
     freq = {}
     for item in lst:
@@ -13,3 +15,14 @@ def count_frequency(lst):
         else:
             freq[item] = 1
     return freq
+
+def dfs(node, state):
+    """Topic: Graph traversal"""
+    """Depth-first search template for binary tree"""
+    if node is None:
+        "Some code"
+        return
+    left = dfs(node.left, state)
+    right = dfs(node.right, state)
+    "Some code"
+    return "Some code"
